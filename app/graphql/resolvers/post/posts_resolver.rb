d794e1025e0a0e7d0fcac9ad2dbbeca7bcea2c25
lakikishorsubba <lakikishor7@gmail.com>
2026-05-11 11:45:36 +0600

    revert filtering query just for reference

--- app/graphql/resolvers/post/posts_resolver.rb
+++ app/graphql/resolvers/post/posts_resolver.rb
@@ -7,6 +7,29 @@ module Resolvers
       argument :title, String, required: false
       argument :body, String, required: false
 
+      # no query object, cant reuse, cant scale
+
+      # def resolve(status: nil, author_id: nil, title: nil, body: nil, **args) # collect all arguement into a ruby hash
+      #   # return  ::Post.all.order(created_at: :asc)
+      #   posts = ::Post.all.order(created_at: :asc)
+      #   # conditonal filtering
+      #   if status.present?
+      #     posts = posts.where(status: status)
+      #   end
+
+      #   if author_id.present?
+      #     posts = posts.where(user_id: author_id)
+      #   end
+      #   if title.present?
+      #     posts = posts.where("title ILIKE ?", "%#{title}%")
+      #   end
+
+      #   if body.present?
+      #     posts = posts.where("body ILIKE ?", "%#{body}%")
+      #   end
+      #   posts
+      # end
+
       def resolve(**args)
         ::PostsQuery.new(params: args, skip: args[:skip]).run
       end
