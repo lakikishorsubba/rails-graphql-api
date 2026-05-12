4b81effebb1cefd277b4e4b6e86ed6dfc5364acd
lakikishorsubba <lakikishor7@gmail.com>
2026-05-12 10:50:45 +0600

    test case: invalid without email and duplicate email

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,7 +1,19 @@
 RSpec.describe(User, type: :model) do # just for redable
   # rspec test cases goes here
-  it "user to be valid" do
+  it("is user to be valid") do
     user = create(:user)
     expect(user).to be_valid # does user.valid?
+  end
+
+  it("is invalid without email") do
+    user = build(:user, email: nil)
+    expect(user).not_to be_valid
+  end
+
+  it("is invalid with duplicate email") do
+    create(:user, email: "test@gmail.com")
+    user = build(:user, email: "test@gmail.com")
+
+    expect(user).not_to be_valid
   end
 end
