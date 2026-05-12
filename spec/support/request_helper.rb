4e136e65cc4f390093e387ac8e41594b54deba04
lakikishorsubba <lakikishor7@gmail.com>
2026-05-12 13:23:35 +0600

    fix: keyword arguement to postitonal argueent

--- spec/support/request_helper.rb
+++ spec/support/request_helper.rb
@@ -1,13 +1,13 @@
 module RequestHelper
-  def graphql_request(query:, variable: {}, user: nil)
-    headers = {}
+  def graphql_request(query, variables: {}, user: nil)
+    headers = { "Content-Type" => "application/json" }
     if user
       token = generate_token(user)
       headers["Authorization"] = "Bearer #{token}" # [] is a method called in the variable header, and Authorization is a key and bearer__ is a value
     end
     post "/graphql",
-      params: { query: query, variables: variable }.to_json,
-      headers: header.merge { "content-type" => "application/json" } # string key value pair
+      params: { query: query, variables: variables }.to_json,
+      headers: headers # string key value pair
   end
 
   def graphql_response
