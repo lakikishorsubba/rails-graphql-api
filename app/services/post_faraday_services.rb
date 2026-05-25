04f0b1fbb94e18d974c647724d3886bb02f85a6f
lakikishorsubba <lakikishor7@gmail.com>
2026-05-25 17:09:02 +0600

    fix faraday service

--- app/services/post_faraday_services.rb
+++ app/services/post_faraday_services.rb
@@ -3,7 +3,7 @@ class PostFaradayServices # class responsible for api request.
   BASE_URL = "https://jsonplaceholder.typicode.com"
 
   # constructor that creates reusable HTTP connection.
-  def initialiaze
+  def initialize
     # instance variable to build farady request.
     @connection = Faraday.new(url: BASE_URL) do |faraday|
       faraday.headers["content-type"] =  "application/json"
@@ -14,12 +14,12 @@ class PostFaradayServices # class responsible for api request.
   end
 
   def fetch_all
-    response = @connection.get("/post") # apped to base url
+    response = @connection.get("/posts") # apped to base url
     JSON.parse(response.body)
   end
 
   def fetch_one(id)
-    response = @connection.get("/post/#{id}")
+    response = @connection.get("/posts/#{id}")
     JSON.parse(response.body)
   end
 end
