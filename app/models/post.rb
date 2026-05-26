71b8633d9be1435ce032dffcf05111d5b01588ab
lakikishorsubba <lakikishor7@gmail.com>
2026-05-26 12:55:28 +0600

    configure aasm state and transition

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,21 @@
 class Post < ApplicationRecord
+  include AASM # bring all the aasm method as instance here
   belongs_to :user
+  validates :status, presence: true
 
   enum :status, { draft: 0, published: 1 }
-  validates :status, presence: true
+  # open the aasm block, direct access to column status using symbol
+  aasm(column: :status, enum: true, whiny_persistence: true) do # aasm is a method and passing arguement to it
+    # state defines the possible valuesA
+    state :draft, initial: true # calling state method with arguement. Can do: state(:draft, initial: true)
+    state :published
+
+  # event defines the transition
+  event(:publish) do
+    transitions(from: :draft, to: :published)
+  end
+  event(:unpublish) do
+    transitions(from: :published, to: :draft)
+  end
+  end
 end
