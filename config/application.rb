ad4bd0e069f70be47ebe3f08006e0d6c45e5f82b
lakikishorsubba <lakikishor7@gmail.com>
2026-05-28 16:22:25 +0600

    add config for solid queue

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ Bundler.require(*Rails.groups)
 module RailsGraphql
   class Application < Rails::Application
     config.middleware.use(Rack::Attack) # config object, middlware and use is a method
+    config.active_job.queue_adapter = :solid_queue
     config.active_record.query_log_tags_enabled = true
     config.active_record.query_log_tags = [
       # Rails query log tags:
