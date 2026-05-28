5f18e24877a2d4cde99be53edf4dc76d10fd0206
lakikishorsubba <lakikishor7@gmail.com>
2026-05-28 16:24:37 +0600

    sync and save the data in db with bot user

--- app/jobs/sync_external_post_job.rb
+++ app/jobs/sync_external_post_job.rb
@@ -3,6 +3,20 @@ class SyncExternalPostJob < ApplicationJob
   queue_as :default
 
   def perform
+    PostsFaradayServices.new.fetch_all.each do |data|
+      Post.find_or_create_by(title: data["title"].to_s.strip) do |post|
+        post.body = data["body"].to_s.strip
+        post.status = :published
+        post.user_id = sync_user.id
+      end
+    end
+  end
+
+  private
+  def sync_user
+    User.find_or_create_by!(email: "sync@gmail.com") do |u|
+      u.password = SecureRandom.hex(32)
+      u.password_confirmation = u.password
+    end
   end
 end
-
