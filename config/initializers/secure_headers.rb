65864445acbdbcb67528af7c410374369a6d9547
lakikishorsubba <lakikishor7@gmail.com>
2026-06-01 15:34:54 +0600

    add secure header

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,3 +1,7 @@
 SecureHeaders::Configuration.default do |config|
+  # disallow iframe
   config.x_frame_options = "SAMEORIGIN" # setter method
+
+  # disallow browser to guess the content type
+  config.x_content_type_options = ("nosniff") # setter method
 end
